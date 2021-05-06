d64d204afb74b26a8692f6a671ddc3cb0bf045ff
danangpurbojati <danangpurbojati@gmail.com>
2021-05-06 20:38:38 +0700

    add websocket and notification

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,8 @@ import '../styles/main.css';
 // import restaurants from '../DATA.json';
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './globals/config';
 
 const app = new App({
   button: document.getElementById('hamburger'),
@@ -17,4 +19,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
