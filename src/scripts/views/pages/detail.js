91ab297057151f904633f5315998451ce020ba3a
danangpurbojati <danangpurbojati@gmail.com>
2021-05-06 11:53:43 +0700

    add detail page

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,51 @@
+import RestaurantDbSource from '../../data/restaurantdb-source';
+import CONFIG from '../../globals/config';
+import UrlParser from '../../routes/url-parser';
+
 const Detail = {
   async render() {
     return `
-        <h2>Detail Page</h2>
-      `;
+      <div id="detail-page" class="detail-page">
+          
+      </div>
+    `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await RestaurantDbSource.detailRestaurant(url.id);
+    console.log(restaurant);
+    const detailPageContainer = document.getElementById('detail-page');
+    const detailRestaurant = `
+      <div class="container">
+          <div class="detail-image-wrapper">
+              <img src="${CONFIG.BASE_IMAGE_URL + restaurant.restaurant.pictureId}" alt="${restaurant.restaurant.name}">
+          </div>
+          <h1 class="detail-page-title">${restaurant.restaurant.name}</h1>
+          <div class="detail-rating-wrapper">
+              <div class="rating-wrapper">
+                  <h3>Rating: </h3>
+                  <p>${restaurant.restaurant.rating}</p>
+              </div>
+              <div class="like-wrapper">
+                  <h3>like</h3>
+              </div>
+          </div>
+          <div class="detail-city-wrapper">
+              <h3>City:</h3>
+              <p>${restaurant.restaurant.city}</p>
+          </div>
+          <div class="detail-city-wrapper">
+              <h3>Address:</h3>
+              <p>${restaurant.restaurant.address}</p>
+          </div>
+          <div class="detail-description-wrapper">
+              <h3>Description:</h3>
+              <p>${restaurant.restaurant.description}</p>
+          </div>
+      </div>
+    `;
+    detailPageContainer.innerHTML = detailRestaurant;
   },
 };
 
--- src/scripts/views/pages/home-page.js
+++ src/scripts/views/pages/home-page.js
@@ -24,6 +24,8 @@ const HomePage = {
                 <div id="menu-content" class="menu-content">
                     
                 </div>
+
+                <a href="/#/detail/23" class="btn more-restaurant-btn">More restaurants</a>
             </div>
         </main>  
     `;
@@ -33,26 +35,28 @@ const HomePage = {
     const restaurants = await RestaurantDbSource.allRestaurants();
     const menuContent = document.getElementById('menu-content');
     let restaurantElement = '';
-    restaurants.forEach((restaurant) => {
-      restaurantElement += `
-        <div class="card">
-            <img class="menu-image" src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="${restaurant.name}">
-            <div class="menu-city">
-                <p>${restaurant.city}</p>
-            </div>
-            <div class="menu-text">
-                <p class="menu-rating">
-                    <strong>Rating: </strong> 
-                    ${restaurant.rating}
-                </p>
-                <h1 class="menu-title">${restaurant.name}</h1>
-                <p class="menu-description">${restaurant.description.substring(0, 100)}. . .</p>
-            </div>
-            <div class="menu-detail">
-                <a aria-label="more detail ${restaurant.name}" class="btn" href="/#/detail/${restaurant.id}">More Detail</a>
-            </div>
-        </div>        
+    restaurants.forEach((restaurant, index) => {
+      if (index < 6) {
+        restaurantElement += `
+            <div class="card">
+                <img class="menu-image" src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="${restaurant.name}">
+                <div class="menu-city">
+                    <p>${restaurant.city}</p>
+                </div>
+                <div class="menu-text">
+                    <p class="menu-rating">
+                        <strong>Rating: </strong> 
+                        ${restaurant.rating}
+                    </p>
+                    <h1 class="menu-title">${restaurant.name}</h1>
+                    <p class="menu-description">${restaurant.description.substring(0, 100)}. . .</p>
+                </div>
+                <div class="menu-detail">
+                    <a aria-label="more detail ${restaurant.name}" class="btn" href="/#/detail/${restaurant.id}">More Detail</a>
+                </div>
+            </div>        
         `;
+      }
     });
 
     menuContent.innerHTML = restaurantElement;
